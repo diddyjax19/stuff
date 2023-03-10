aa0c943d4fd1a99b93432bdfd56389ce0fffb433
diddyjax19 <adetobi59@gmail.com>
2023-03-10 00:35:38 +0000

    f

--- ecomprj/env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://aifymxjr:XESphqT7VmYlfNbVUfzLxYXxFiZhsVCp@trumpet.db.elephantsql.com/aifymxjr"
-os.environ["SECRET_KEY"] = "smzAg4tCsjZgoKjeQD4lgWu9aG9UiIC4"
